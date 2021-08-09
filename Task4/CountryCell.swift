fb5922c6e39a855b63579e78146f1c9a3b3ae790
LV <vvvlizagub@gmail.com>
2021-08-09 18:09:40 +0300

    fixed code

--- Task4/CountryCell.swift
+++ Task4/CountryCell.swift
@@ -16,7 +16,7 @@ class CountryCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-s
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         // Configure the view for the selected state
